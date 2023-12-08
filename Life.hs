841aa330536d6ffa57564ce983b2e4aeca77285c
pingshiyu <pingshiyu@gmail.com>
2023-12-08 18:01:53 +0000

    some modifications on tabbing, attempts at making move

--- Life.hs
+++ Life.hs
@@ -16,6 +16,11 @@ instance Functor (Store s) where
 instance Comonad (Store s) where
   extract :: Store s a -> a
   extract (Store f s) = f s
+
+  -- given a Store (s -> a) s, which accesses the `s` at a certain point,
+  -- and so Store s a is isomorphic to the "index" of s. some kind of s[i]
+  -- duplicate is such that for each "st", "extract (duplicate st) === st"
+  -- duplicate (s[i]) ~= (s, s[i]), so it kind of breaks apart s[i] and its arguments
   duplicate :: Store s a -> Store s (Store s a)
   duplicate (Store f s) = Store (Store f) s
 
@@ -26,18 +31,17 @@ data GameState = Empty | Alive | Dead
 type Game = [[GameState]]
 
 put :: GameState -> Int -> Int -> Game -> Game
-put gs i j g = [
-    [ if i' /= i || j' /= j 
-        then g !! i' !! j' 
-        else gs 
-        | j' <- [1 .. j]
-    ]   | i' <- [1 .. i]]
+put gs i j g =
+  [ if i' /= i || j' /= j 
+    then g !! i' !! j' 
+    else gs 
+    | j' <- [1 .. j], i' <- [1 .. i]]
 
 initState :: Int -> Int -> Game
 initState n m = replicate n (replicate m Empty)
 
 move :: Game -> Game
--- move = access i j 
+move g = [access g i j | i <- [1 .. length g], j <- [1 .. length g]]
 
 main :: IO ()
 main = print "Life."
